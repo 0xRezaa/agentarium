846789074782d48332bb9cf2cd54e935cfdad462
0xRezaa <92025027+0xRezaa@users.noreply.github.com>
2026-05-25 02:34:27 +0200

    chore: eslint fix

--- packages/model-webllm/src/index.ts
+++ packages/model-webllm/src/index.ts
@@ -1,6 +1,5 @@
-import type { MLCEngineConfig } from "@mlc-ai/web-llm";
 import type { ModelAdapter } from "#core/model/types";
 
-export function createWebLLMAdapter({}: MLCEngineConfig): ModelAdapter {
+export function createWebLLMAdapter(): ModelAdapter {
   throw new Error("Not implemented yet");
 }
