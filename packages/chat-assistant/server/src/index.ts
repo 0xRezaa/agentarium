91d8ca72d6ff14be08fe3a05939205a21049a21f
0xRezaa <92025027+0xRezaa@users.noreply.github.com>
2026-05-30 04:18:19 +0200

    chore: format and lint fix

--- packages/chat-assistant/server/src/index.ts
+++ packages/chat-assistant/server/src/index.ts
@@ -1,16 +1,19 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
-const port = Number.parseInt(process.env['PORT'] ?? '3000', 10)
+const app = new Hono();
+const port = Number.parseInt(process.env["PORT"] ?? "3000", 10);
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-serve({
-  fetch: app.fetch,
-  port,
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+serve(
+  {
+    fetch: app.fetch,
+    port,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${String(info.port)}`);
+  },
+);
