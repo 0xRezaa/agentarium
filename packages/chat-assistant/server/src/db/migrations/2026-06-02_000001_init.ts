e7c6be917a80375a0f576b2f2ade8af5cb8e2f87
0xRezaa <92025027+0xRezaa@users.noreply.github.com>
2026-06-02 17:03:39 +0200

    chore: add placeholder migration

---
+++ packages/chat-assistant/server/src/db/migrations/2026-06-02_000001_init.ts
@@ -0,0 +1,7 @@
+import type { Kysely } from "kysely";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export async function up(_db: Kysely<unknown>): Promise<void> {}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export async function down(_db: Kysely<unknown>): Promise<void> {}
