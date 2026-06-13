879a78d0f1d1436ac6fff8b88357bc1976d991da
0xRezaa <92025027+0xRezaa@users.noreply.github.com>
2026-06-13 03:48:24 +0200

    fix: export proper node module

---
+++ packages/core/src/test-kit/index.ts
@@ -0,0 +1,4 @@
+export * from "./utils.js";
+export * from "./model-messages.js";
+export * from "./mock-model-adapters/mock-model-adapter.js";
+export * from "./mock-model-adapters/mock-tool-call-model-adapter.js";
